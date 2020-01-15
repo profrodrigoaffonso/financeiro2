39ecb48e825ac3e4a8f2869f3fa210a70b351c69
rodrigo <profrodrigoaffonso@gmail.com>
2020-01-15 18:41:16 -0300

    Erros

--- resources/views/bancos/create.blade.php
+++ resources/views/bancos/create.blade.php
@@ -6,10 +6,16 @@
     <div class="form-group">
       <label for="exampleInputEmail1">Código</label>
       <input type="text" class="form-control" id="codigo" name="codigo">
+      @if($errors->has('codigo'))
+      Digite o código
+      @endif
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Nome</label>
       <input type="text" class="form-control" id="nome" name="nome">
+      @if($errors->has('nome'))
+      Digite o nome
+      @endif
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Agência</label>
