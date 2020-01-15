ed73cac91369859a4690a089ab378002aebff2f0
rodrigo <profrodrigoaffonso@gmail.com>
2020-01-15 19:35:05 -0300

    Correção

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <form class="form-signin" method="POST" action="{{ route('login.login') }}">
     @csrf
     <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Entrar no sitema</h1>
+    <h1 class="h3 mb-3 font-weight-normal">Entrar no sistema</h1>
     <label for="email" class="sr-only">E-mail</label>
     <input type="email" name="email" id="email" class="form-control" placeholder="E-mail" required autofocus>
     <label for="password" class="sr-only">Senha</label>
