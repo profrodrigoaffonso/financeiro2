94f20783f8f7ad7b1e2fffd8efa64a99ec2cced0
rodrigo <profrodrigoaffonso@gmail.com>
2020-01-19 13:17:23 -0300

    Menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,7 +17,7 @@
   <body>
     <div class="container-fluid">
         <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 class="my-0 mr-md-auto font-weight-normal">Nome da companhia</h5>
+            <h5 class="my-0 mr-md-auto font-weight-normal">Financeiro - Admin</h5>
             <nav class="my-2 my-md-0 mr-md-3">
               <a class="p-2 text-dark" href="{{ route('pagamentos.index') }}">Pagamentos</a>
               <a class="p-2 text-dark" href="{{ route('bancos.index') }}">Bancos</a>
@@ -25,7 +25,7 @@
               <a class="p-2 text-dark" href="{{ route('forma_pagamentos.index') }}">Forma de pagamentos</a>
               <a class="p-2 text-dark" href="{{ route('login.logout') }}">Sair</a>
             </nav>
-            <a class="btn btn-outline-primary" href="#">Cadastro</a>
+            {{-- <a class="btn btn-outline-primary" href="#">Cadastro</a> --}}
           </div>
         @yield('content')
     </div>
