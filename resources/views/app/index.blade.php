8ce31d46c0158416454fd2c3f29fbd2ea5699bf5
Rodrigo Affonso <profrodrigoaffonso@gmail.com>
2020-02-19 20:54:41 -0300

    Aplicativos

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <h1 class="text-center">App</h1><br><br>
-    <a href="/inserir"><button type="button" class="btn btn-primary btn-lg btn-block">Inserir</button></a><br><br>
-    <a href="/saques"><button type="button" class="btn btn-primary btn-lg btn-block">Saques</button></a>
+    <a href="{{ route('pagamentos.inserir') }}"><button type="button" class="btn btn-primary btn-lg btn-block">Inserir</button></a><br><br>
+    <a href="{{ route('saques.inserir') }}"><button type="button" class="btn btn-primary btn-lg btn-block">Saques</button></a>
 
 
 @endsection
